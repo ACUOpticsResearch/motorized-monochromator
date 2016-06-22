01edb1c6360aaa13b1e7bb8d500610f733991af1
jtb14b <jtb14b@acu.edu>
2016-06-22 16:47:15 -0500

    Completely removed real time output functionality.

--- scanRoutineRev1.py
+++ scanRoutineRev1.py
@@ -11,13 +11,13 @@
 
 from monochromator import Mono
 from multimeter import Multimeter
-#import matplotlib #only needed if realTimeOut flag is set to true
+import matplotlib #only needed if realTimeOut flag is set to true
 import serial #necessary?
 import time #necessary?
 import visa
 
 #startWvl and endWvl must have values between 2500 and 9950
-def scanRoutine(port, startWvl, endWvl, numPtsPerWvl, stepSize, fileName, realTimeOut=False):
+def scanRoutine(port, startWvl, endWvl, numPtsPerWvl, stepSize, fileName):
     a = Mono(port)
     b = Multimeter(visa.ResourceManager())
     #time.sleep(4)
@@ -27,28 +27,6 @@ def scanRoutine(port, startWvl, endWvl, numPtsPerWvl, stepSize, fileName, realTi
     if startWvl > endWvl:
         stepSize = -stepSize
 
-#do we want to plot data in real time? How much should be done here?
-    if realTimeOut:
-        import numpy as np
-        import matplotlib.pyplot as plt
-
-		#define pltWvl and pltAmp arrays here
-        pltWvl = [startWvl-stepSize]+range(startWvl, endWvl, stepSize)
-        pltAmp = [b.measureVoltageDC()]
-        #then:
-        line, = plt.plot(np.array(pltWvl)[0], np.array(pltAmp)[0], 'b-', linewidth=2)
-        dashes = [10, 5, 100, 5] # 10 points on, 5 off, 100 on, 5 off
-        line.set_dashes(dashes)
-
-        if stepSize > 0:
-            plt.axis([startWvl,endWvl,0,.5])
-        else:
-            plt.axis([endWvl,startWvl,0,.5])
-
-        plt.ion()
-        plt.show()
-        curIndex = 1
-
     my_file = open(fileName, "w")
     a.goToWavelength(startWvl) #should I go here before the loop?
     print range(startWvl, endWvl, stepSize)
@@ -56,32 +34,17 @@ def scanRoutine(port, startWvl, endWvl, numPtsPerWvl, stepSize, fileName, realTi
         #start at start wavelength, and increment by stepSize taking numPtsPerWvl data points at each wvl
         for wvl in range(startWvl, endWvl, stepSize):
             a.goToWavelength(wvl)
-            print ("Measuring at " + str(wvl) + " Angstroms")
             buf = [] #buffer for storing numPtsPerWvl data points
             for i in range(numPtsPerWvl):
                 value = b.measureVoltageDC()
                 buf.append(value)
-                print value
                 my_file.write(str(wvl) + "," + str(value) + "\n")
 
             data[wvl] = buf
 
-            if realTimeOut:
-                #get average of buf and store in pltAmp for real-time
-                sum = 0
-                for n in buf:
-                    sum += n
-                pltAmp.append(sum)
-
-                curIndex +=1
-
-                line, = plt.plot(np.array(pltWvl)[1:curIndex], np.array(pltAmp)[1:curIndex], 'b-', linewidth=2)
-                plt.draw()
-
         #include endWvl values, too
         a.goToWavelength(endWvl)
         buf = [] #buffer for storing numPtsPerWvl data points
-        print "Measuring at " + str(endWvl) + " Angstroms"
         for i in range(numPtsPerWvl):
             value = b.measureVoltageDC()
             buf.append(value)
