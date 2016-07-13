59aa9a0a062bb9142e8b7cb59b0372ddea52b161
jtb14b <jtb14b@acu.edu>
2016-07-13 16:01:27 -0500

    Cleaned it up.

--- monochromator.py
+++ monochromator.py
@@ -33,7 +33,7 @@ class Mono(object):
         #variable stored in memory are synced on startup.
         self.getWavelengthFromMemory()
 
-    def __wrt__(self, input):
+    def _wrt(self, input):
         #Sends the imput command over serial to the Arduino. Then, it waits
         #the Arduino sends a response back to say that it has received the
         #command.
@@ -42,22 +42,30 @@ class Mono(object):
             pass
         time.sleep(.01)
 
-    def __rd__(self):
+    def _rd(self):
         #Reads everything waiting in the buffer, then strips it of
         #unnecessary white space, casts it as a string, and then returns it.
         return str(self.serPort.read(self.serPort.in_waiting)).strip()
 
-        #After __wrt__() is called, this function should be called before
-        #calling __wrt__() again. This allows Python and the Arduino to
+        #After _wrt() is called, this function should be called before
+        #calling _wrt() again. This allows Python and the Arduino to
         #properly wait for eachother to finish.
+
+    def _roundToStepSize(self, value):
+        diff = (value-4000) % .3125
+        if diff <= .3125/2:
+            return (value - diff)
+        else:
+            return (value - diff + .3125)
+        return value
 
     def setWavelengthInMemory(self, inputWavelength):
         #Sends the command that corresponds to this method to the Arduino,
         #specifies the wavelength, and then returns the same wavelength.
-        self.__wrt__(str(1))
+        self._wrt(str(1))
         self.serPort.reset_input_buffer()
-        self.__wrt__(str(inputWavelength))
-        wvl = self.__rd__()                     #This read is necessary because the Arduino sends a response back to Python once it is done
+        self._wrt(str(inputWavelength))
+        wvl = self._rd()                     #This read is necessary because the Arduino sends a response back to Python once it is done
         self.serPort.reset_input_buffer()
         return wvl
 
@@ -65,47 +73,51 @@ class Mono(object):
         #Sends the command that corresponds to this method to the Arduino,
         #and then reads the Serial buffer, and returns the wavelength that
         #is stored in memory
-        self.__wrt__('2')
-        self.storedWavelength = self.__rd__()
+        self._wrt('2')
+        self.storedWavelength = self._rd()
         return self.storedWavelength
 
     def goToWavelength(self, chooseWavelengthInAngstroms):
         #First checks if the given wavelength is within the possible range
-        self.chooseWavelengthInAngstroms = chooseWavelengthInAngstroms
+        roundedWavelength = self._roundToStepSize(chooseWavelengthInAngstroms)
+        if roundedWavelength != chooseWavelengthInAngstroms:
+            chooseWavelengthInAngstroms = roundedWavelength
+            print "Rounded to "  + roundedWavelength
+
         if chooseWavelengthInAngstroms > 9950 or chooseWavelengthInAngstroms < 2500:
             raise ValueError('Invalid monochromator wavelength')  #raises an error if it is out of range
         else:
             #Sends the command to the Arduino, reads its affirmative response,
             #then sends the wavelength, reads its response again, and then
             #returns it.
-            self.__wrt__('3')
-            self.__rd__()
-            self.__wrt__(str(chooseWavelengthInAngstroms))
-            out = self.__rd__()
+            self._wrt('3')
+            self._rd()
+            self._wrt(str(chooseWavelengthInAngstroms))
+            out = self._rd()
             return out
 
     def stepUp(self):
         #Sends the corresponding command and then returns it.
-        self.__wrt__('4')
-        return self.__rd__()
+        self._wrt('4')
+        return self._rd()
 
     def stepDown(self):
         #Same as stepUp(), except for it sends the command corresponding to
         #the stepDown function in the Arduino.
-        self.__wrt__('5')
-        return self.__rd__()
+        self._wrt('5')
+        return self._rd()
 
     def getWavelength(self):
         #Sends the request to the Arduino, reads its response, updates the
         #current variable on the Python side, and then returns it.
-        self.__wrt__('6')
-        currentWavelength = self.__rd__()
+        self._wrt('6')
+        currentWavelength = self._rd()
         self.currentWavelength = currentWavelength
         return currentWavelength
 
     def release(self):
-        self.__wrt__('7')
-        return self.__rd__()
+        self._wrt('7')
+        return self._rd()
 
     def closePort(self):
         #Tells the Serial port to close
